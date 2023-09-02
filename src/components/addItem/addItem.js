401bcf6ecf3f85e41cba9308eaafa615325d3c1d
ziad-elshihy <elshihyziad@gmail.com>
2023-09-03 00:05:00 +0300

    added quantity tab

--- src/components/addItem/addItem.js
+++ src/components/addItem/addItem.js
@@ -1,38 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class addItem extends Component {
-    state = {
-        product: '',
-        price: ''
-    }
+   state = {
+      product: '',
+      price: '',
+      quantity: 1
+   }
 
-    handleChange = (e) => {
-        console.log(e.target.id + ": " + e.target.value)
-        this.setState({
-            [e.target.id]: e.target.value
-        })
-    }
+   handleChange = (e) => {
+      console.log(e.target.id + ": " + e.target.value)
+      this.setState({
+         [e.target.id]: e.target.value
+      })
+   }
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        this.props.add(this.state)
-        this.setState({
-            product: '',
-            price: ''
-        })
-    }
+   handleSubmit = (e) => {
+      e.preventDefault()
+      this.props.add(this.state)
+      this.setState({
+         product: '',
+         price: '',
+         quantity: 1
+      })
+   }
 
-    render(){
-        return(
-            <div className="item">
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" value={this.state.product} placeholder="Enter Product" id="product" onChange={this.handleChange} required/>
-                    <input type="number" value={this.state.price} placeholder="Enter Price" id="price" onChange={this.handleChange} required/>
-                    <input type="submit" value="Add"/>
-                </form>
-            </div>
-        )
-    }
+   render() {
+      return (
+         <div className="item">
+            <form onSubmit={this.handleSubmit}>
+               <input type="text" value={this.state.product} placeholder="Enter Product" id="product" onChange={this.handleChange} required />
+               <input type="number" value={this.state.price} placeholder="Enter Price" id="price" onChange={this.handleChange} required />
+               <input type="submit" value="Add" />
+            </form>
+         </div>
+      )
+   }
 }
 
 export default addItem;
--- src/components/item/items.js
+++ src/components/item/items.js
@@ -1,31 +1,37 @@
 import React from 'react';
 
 const Items = (props) => {
-    const {items, del} = props;
-    let length = items.length
-    const ListItem = length ? (
-        items.map(item => {
-            return(
-                <div key={item.id} className="item">
-                    <p>{item.product}</p>
-                    <p>{item.price}</p>
-                    <p className="delete" onClick={() => del(item.id)}>&times;</p>
-                </div>
-            )
-        })
-    ) : (
-        <div className="text">There are no items, Try to add some.</div>
-    )
-    return (
-        <div>
-            <div className="header item">
-                <p>Product</p>
-                <p>Price</p>
-                <p>Edit</p>
+   const { items, del , increaseItem , decreaseItem } = props;
+   let length = items.length
+   const ListItem = length ? (
+      items.map(item => {
+         return (
+            <div key={item.id} className="item width">
+               <p>{item.product}</p>
+               <p>{item.price}</p>
+               <p className="delete" onClick={() => del(item.id)}>&times;</p>
+               <span className="Change">
+                  <p className='quantity'>{item.quantity}</p>
+                  <p onClick={() => increaseItem(item.id)}>&#43;</p>
+                  <p onClick={() => decreaseItem(item.id)}>&minus;</p>
+               </span>
             </div>
-            {ListItem}
-        </div>
-    )
+         )
+      })
+   ) : (
+      <div className="text">There are no items, Try to add some.</div>
+   )
+   return (
+      <div>
+         <div className="header item">
+            <p>Product</p>
+            <p>Price</p>
+            <p>Edit</p>
+            <p>Quantity</p>
+         </div>
+         {ListItem}
+      </div>
+   )
 }
 
 export default Items
